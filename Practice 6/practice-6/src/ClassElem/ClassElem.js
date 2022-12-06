fc4a28dad7370a888d22fe45028fc1a1d45a23e0
pashacyborg <pashacyborg2@gmail.com>
2022-12-06 14:11:17 +0300

    pr6

---
+++ Practice 6/practice-6/src/ClassElem/ClassElem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class ClassElem extends React.Component {
+    render () {
+        return (
+            <div>
+                <input type="text" placeholder="text 2"/>
+                <input type="submit" value="Button 2"/>
+            </div>
+        );
+    }
+}
---
+++ Practice 6/practice-6/src/FuncElem/FuncElem.js
@@ -0,0 +1,8 @@
+export function FuncElem () {
+    return (
+        <div>
+            <input type="text" placeholder="text 1"/>
+                <input type="submit" value="Button 1"/>
+        </div>
+    );
+}
